d72f76da0000a349864be537f65d0884f07fd302
ChristianH545 <christianvenezuelahome@gmail.com>
2021-12-20 13:44:02 -0300

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+// interaccion con el contenedor de las tarjetas
+
+$(".btn_card").click(function () {
+  const card = $("#contenedor");
+  card.slideUp(1000).slideDown(10000);
+});
+// animacion con el logo
+$(".logo").fadeOut(2000).fadeToggle(2000);
+
+// animacion de la bienvenida de la pagina
+$(".bienvenida").fadeOut(3000);
+
+$;
